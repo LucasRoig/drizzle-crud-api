c7583109b025badfc82a62b949690aca4174d0b4
Lucas Roig <lucas1.roig@gmail.com>
2025-03-02 12:57:11 +0100

    feat(drizzle-repo): add filters

--- packages/data-access/src/lib/drizzle-repo.ts
+++ packages/data-access/src/lib/drizzle-repo.ts
@@ -1,13 +1,39 @@
 import { getTableColumns, type Table } from "drizzle-orm";
-import type { PgDatabase } from "drizzle-orm/pg-core";
+import  type { PgDatabase } from "drizzle-orm/pg-core";
+import { drizzleSchema } from "@repo/database";
+import { type FiltersCore, toDrizzleWhereQuery } from "./filters";
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
 type AnyPgDatabase = PgDatabase<any, any>;
 
+// biome-ignore lint/correctness/noUnusedVariables: <explanation>
+function test(database: AnyPgDatabase) {
+  const table = drizzleSchema.movies
+  const query = database.select({
+    ...getTableColumns(table),
+  }).from(table).$dynamic()
+
+  query.execute()
+}
+
+type FindManyArgs<T extends Table> = {
+  where?: FiltersCore<T>;
+};
 export function buildFindMany<TDatabase extends AnyPgDatabase, TTable extends Table>(database: TDatabase, table: TTable) {
-  return () => {
-    return database.select({
+  return (args?: FindManyArgs<TTable>) => {
+    const query = database.select({
       ...getTableColumns(table),
-    }).from(table as Table);
+    }).from(table as Table)
+    .$dynamic();
+
+    if (args?.where) {
+      query.where(toDrizzleWhereQuery(table, args.where));
+    }
+
+    const results = query.execute();
+
+    //By using the $dynamic() method, we loose the correct inferrence of the return type
+    //So we need to cast it to the correct type
+    return results as unknown as typeof drizzleSchema.movies.$inferSelect[];
   }
 }
 
--- packages/data-access/test/repo.test.ts
+++ packages/data-access/test/repo.test.ts
@@ -7,7 +7,7 @@ const schema = drizzleSchema;
 
 test("findAll", async () => {
   const { drizzleClient: client} = await getPostgresContainer();
-  const movies: typeof schema.movies.$inferInsert[] = [
+  const movies = [
     { title: "The Matrix", date: new Date() },
     { title: "The Matrix Reloaded", date: new Date() },
     { title: "The Matrix Revolutions", date: new Date() },
@@ -15,9 +15,29 @@ test("findAll", async () => {
 
   await client.insert(schema.movies).values(movies);
   const movieRepository = buildRepository(client, schema.movies);
+
   const allMovies = await movieRepository.findMany();
   expect(allMovies.length).toBe(3);
-  expect(allMovies[0]?.title).toBe("The Matrix");
-  expect(allMovies[1]?.title).toBe("The Matrix Reloaded");
-  expect(allMovies[2]?.title).toBe("The Matrix Revolutions");
+  expect(allMovies[0]?.title).toBe(movies[0]?.title);
+  expect(allMovies[1]?.title).toBe(movies[1]?.title);
+  expect(allMovies[2]?.title).toBe(movies[2]?.title);
+
+  const filterByTitle = await movieRepository.findMany({
+    where: {
+      title: {
+        eq: "The Matrix",
+      },
+    },
+  });
+  expect(filterByTitle.length).toBe(1);
+  expect(filterByTitle[0]?.title).toBe(movies[0]?.title);
+
+  const filterByTitleIlike = await movieRepository.findMany({
+    where: {
+      title: {
+        ilike: "%Matrix%",
+      },
+    },
+  });
+  expect(filterByTitleIlike.length).toBe(3);
 })
